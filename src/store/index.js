f543689b6bd42865a981e69ec789f0d389c71543
Maffinca69 <maffinca@gmail.com>
2021-07-10 23:59:33 +0400

    Remove unused code

--- src/store/index.js
+++ src/store/index.js
@@ -77,23 +77,6 @@ export default new Vuex.Store({
                 }
             })
         },
-        /**
-         *
-         * @param key
-         * @param value
-         * @param ttl - minutes
-         */
-        saveValueWithExpiry: ({key, value, ttl}) => {
-            const now = new Date()
-
-            // `item` is an object which contains the original value
-            // as well as the time when it's supposed to expire
-            const item = {
-                value: value,
-                expiry: now.getTime() + ttl * 60,
-            }
-            localStorage.setItem(key, JSON.stringify(item))
-        }
     },
     modules: {},
     getters: {
